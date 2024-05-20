1e9628b174534ab8423b211a85341427338f4632
Eric Lluis <ericlluis84@gmail.com>
2024-05-20 19:09:28 +0200

    Test PingController

---
+++ src/server/PingController/PingController.test.ts
@@ -0,0 +1,14 @@
+import request from "supertest";
+import { app } from "../app";
+
+describe("Given a GET/ endpoint", () => {
+  describe("When it receives a request", () => {
+    test("Then it should respond status 200 and a message '🏓pong'", async () => {
+      const response = await request(app).get("/").expect(200);
+
+      const body = response.body as { message: string };
+
+      expect(body.message).toBe("🏓pong");
+    });
+  });
+});
