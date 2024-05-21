0e9cdaf5b2ab601e9398544efd4e45f2d19ebeab
Eric Lluis <ericlluis84@gmail.com>
2024-05-21 18:48:42 +0200

    Test PingController

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,15 +2,14 @@ import express from "express";
 import morgan from "morgan";
 import generalError from "./middlewares/generalError.js";
 import notFoundError from "./middlewares/notFoundError.js";
-import PingController from "./PingController/PingController.js";
+import PingController from "./ping/PingController.js";
+import pingRouter from "./ping/router/pingRouter.js";
 
 export const app = express();
 
 app.use(morgan("dev"));
 
-const pingController = new PingController();
-
-app.get("/", pingController.getPong);
+app.get("/", pingRouter);
 
 app.use(notFoundError);
 app.use(generalError);
